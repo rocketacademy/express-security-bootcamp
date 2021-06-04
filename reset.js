883873d685d2b71108e54f078ad019d1cab55d23
awongh <akira@awongh.com>
2021-06-04 17:53:17 +0800

    empty db on reset, but refill db with data from seed

--- reset.js
+++ reset.js
@@ -26,6 +26,31 @@ client.connect();
 
 client.query('DELETE FROM users').then(result=>{
   client.query('DELETE FROM cats').then(result=>{
+
+
+    const queryStrings = [
+      "INSERT INTO users (email, password) VALUES ('kai@ra.com', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2')",
+
+      "INSERT INTO users (email, password) VALUES ('jai@ra.com', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2')",
+
+      "INSERT INTO cats (name, type, user_id) VALUES ('Mr. Snuggles', 'Calico', 1)",
+      "INSERT INTO cats (name, type, user_id) VALUES ('Jake AlPurrrrtsen', 'Bambino', 1)",
+      "INSERT INTO cats (name, type, user_id) VALUES ('Furry Mc Furmeister', 'Persian', 1)",
+      "INSERT INTO cats (name, type, user_id) VALUES ('kai', 'LaPerm', 2)"
+    ];
+
+    const resultPromises = [];
+
+    for( let i=0; i<queryStrings.length; i++ ){
+      resultPromises.push( client.query(queryStrings[i]) );
+    }
+
+    Promise.all( resultPromises ).then( results => {
+
+      console.log( results );
+      client.query('DELETE FROM cats')
+    });
+
     client.end();
   });
 }).catch(error =>{
